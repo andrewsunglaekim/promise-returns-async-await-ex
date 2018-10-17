503fab2d19e651332bb37846216b2c5417921765
Andy <andrew.sunglae.kim@gmail.com>
2018-10-16 22:18:31 -0400

    finishes ex1 solution

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ const forecastView = new ForecastView();
 input.addEventListener('keypress', async function(e) {
   if (e.keyCode === 13){
     console.log(e.target.value);
-    const forecast = new Forecast({
-      currently: {
-        temperature: 59,
-        summary: 'Rainy',
-        icon: 'rain'
-      }
+    getLatLng(e.target.value).then(({lat, lng}) => {
+      const forecast = new Forecast({
+        currently: {
+          temperature: 59,
+          summary: `${lat},${lng}`,
+          icon: 'rain'
+        }
+      })
+      forecastView.setForecast(forecast)
     })
-    forecastView.setForecast(forecast)
   }
 })
+
+function getLatLng(location){
+  return axios.get(`http://www.mapquestapi.com/geocoding/v1/address?key=<api-key-here>&location=${location}`)
+    .then((res) => {
+      return res.data.results[0].locations[0].latLng
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
